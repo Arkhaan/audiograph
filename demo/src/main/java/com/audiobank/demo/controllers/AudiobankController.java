2dfb6117763e541eee33abbcd4b443e46f77da3e
Arkhaan <martin.binet@laposte.net>
2022-12-25 22:22:49 +0100

    Add audio list display + small menu style changes

--- demo/src/main/java/com/audiobank/demo/controllers/AudiobankController.java
+++ demo/src/main/java/com/audiobank/demo/controllers/AudiobankController.java
@@ -2,10 +2,12 @@ package com.audiobank.demo.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
 
 import com.audiobank.demo.repositories.AudiofileRepository;
 import com.audiobank.demo.models.Audiofile;
@@ -15,16 +17,22 @@ public class AudiobankController {
 
     AudiofileRepository repo;
 
+    @Value("${files.path}")
+    private String filesPath;
+
     public AudiobankController(AudiofileRepository repo)
     {
         this.repo = repo;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String helloWorld()
+    public String helloWorld(Model model)
     {
+        model.addAttribute("filesPath", filesPath);
         List<Audiofile> audiofiles = repo.findAll();
-        System.out.println(audiofiles.get(0).getFile_name());
+        model.addAttribute("audiofiles", audiofiles);
+        //System.out.println(audiofiles.get(0).getFile_name());
+        //System.out.println(filesPath);
         return "main-menu";
     }
     
